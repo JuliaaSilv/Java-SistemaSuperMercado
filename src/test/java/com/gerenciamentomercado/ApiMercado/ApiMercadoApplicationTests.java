02073d6df46a0c4d2e68e1086d8d470d1497a7e3
AnnaJulia <julia210320055@gmail.com>
2025-04-14 17:38:56 -0300

    ✨ Commit inicial do projeto

---
+++ src/test/java/com/gerenciamentomercado/ApiMercado/ApiMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentomercado.ApiMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
